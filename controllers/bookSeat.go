7b274fec6bc8a500f393b19898e4e7201ec28dc7
Boruiqiao <39862938+qiaoborui@users.noreply.github.com>
2023-06-27 05:45:11 +0000

    Temporary Storage

--- controllers/bookSeat.go
+++ controllers/bookSeat.go
@@ -1,15 +1,36 @@
 package controllers
 
 import (
-	"QueryLib/utils"
+	"QueryLib/services"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+type BookSeatBody struct{
+	Username string `json:"username"`
+	Password  string `json:"password"`
+	SeatID    string `json:"seatID"`
+	Floor	 string `json:"floor"`
+	Area	  string `json:"area"`
+}
 func BookSeat(c *gin.Context) {
-	fmt.Println("Hello World!")
+	body,err := io.ReadAll(c.Request.Body)
+	fmt.Println(body)
+	var bookSeatBody BookSeatBody
+	err = json.Unmarshal(body, &bookSeatBody)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Error unmarshalling request body",
+		})
+		return
+	}
+
 	user := utils.NewSession()
-	err := user.Login("user", "passwd")
+	err = user.Login(bookSeatBody.Username, bookSeatBody.Password)
 	if err != nil {
 		fmt.Println(err)
 	}
